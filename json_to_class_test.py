350a63cc8bbbf6952071bf5ff3f272062ae8fd1d
clemence.robinletellier@gmail.com <clemence.robinletellier@gmail.com>
2020-03-05 20:14:29 +0100

    update test json

--- json_to_class_test.py
+++ json_to_class_test.py
@@ -3,7 +3,7 @@
 
 import json
 import requests
-from api_openfoodfacts.api_connection import APIInformation
+from collections import namedtuple
 
 
 class NewAPIInformation:
@@ -28,77 +28,3 @@ class Categories(object):
     id = ""
     name = ""
 
-
-class Products(object):
-    id = ""
-    name = ""
-    id_category = ""
-    brands = ""
-    nutriscore = ""
-    link = ""
-    store = ""
-
-
-class Favorites(object):
-    id = ""
-    name_source_product = ""
-    nutriscore_source_product = ""
-    name_alternative_product = ""
-    nutriscore_alternative_product = ""
-    store_alternative_product = ""
-    link_alternative_product = ""
-
-
-def insert_data_to_products(jsondata):
-    p = Products()
-    p.__dict__.update(jsondata)
-    return p
-
-
-def insert_data_to_categories(jsondata):
-    c = Categories()
-    c.__dict__.update(jsondata)
-    return c
-
-
-def insert_data_to_favorites(jsondata):
-    f = Favorites()
-    f.__dict__.update(jsondata)
-    return f
-
-
-def new_get_products():
-    """
-    Get a list of products with a request via the API.
-    """
-
-    for page in range(NewAPIInformation.PAGE_MIN, NewAPIInformation.PAGE_MAX):
-        NewAPIInformation.PARAMETERS['page'] = page
-        # Make the request via the API.
-
-        # <class 'requests.models.Response'>
-        # response n'a pas t'attribut read
-        products_request = requests.get(NewAPIInformation.PRODUCTS_LINK,
-                                        params=NewAPIInformation.PARAMETERS)
-
-        # <class 'dict'>
-        products = products_request.json()
-        # sort needed information
-        for element in products['products']:
-            if not all(tag in element for tag in (
-                    "product_name", "brands", "nutrition_grade_fr", "url", "stores",
-                    "categories")):
-                pass
-            else:
-                # convert python dict to str
-                print(type(products)) # dict
-                json_products = json.dumps(products) # str
-                ins = json.loads(json_products, object_hook=insert_data_to_products)
-                ins.get('product_name', 'brands', 'nutrition_grade_fr', 'url', 'stores',
-                        ""       'categories')
-                # AttributeError: 'Products' object has no attribute 'get'
-        page += 1
-
-
-new_get_products()
-# print(Products.id)
